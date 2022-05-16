7470a889ae336ed97c4ded5ff727138bb57b7d33
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-05-16 20:52:18 +0300

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+fs.writeFile(path.join(__dirname, 'text.txt'), '', (err) => {
+  if (err) throw err;
+});
+
+stdout.write(`Please, enter the text for add to 'text.txt': \n`);
+
+stdin.on('data', (data) => {
+  if (data.toString().slice(0, 4) === 'exit') {
+    process.exit();
+  }
+  fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+process.on('exit', (code) => {
+  if (code === 0) {
+    stdout.write(`Good luck on the course!`);
+  } else {
+    stderr.write(`Error ${code}`);
+  }
+});
