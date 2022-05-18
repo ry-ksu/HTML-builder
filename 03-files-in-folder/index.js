6d72af060dd455619213469f260b3d60d3861997
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-05-18 23:04:01 +0300

    fix: rename 'error' for 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,15 +4,15 @@ const directory = path.resolve(__dirname, '.\\secret-folder');
 
 fs.readdir(directory, { withFileTypes: true }, (err, files) => {
   if (err) {
-    console.log('err');
+    console.log(err);
   } else {
     files.forEach((file) => {
       if (file.isFile() === true) {
         fs.stat(
           path.resolve(__dirname, '.\\secret-folder', file.name),
-          (error, stats) => {
-            if (error) {
-              console.log(error);
+          (err, stats) => {
+            if (err) {
+              console.log(err);
             } else {
               console.log(
                 `< ${file.name.split('.')[0]} - ${path.extname(file.name).slice(1)} - ${stats.size}byte >`
