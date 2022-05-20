ca08dd4034ae8183109d82e772e23719a8a22171
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-05-20 14:56:38 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const path = require('path');
+const fs = require('fs');
 
+const copyFolder = () => {
+  fs.mkdir(
+    path.join(__dirname, '.\\files-copy'),
+    { recursive: true },
+    (err) => {
+      if (err) {
+        throw err;
+      } else {
+        fs.readdir(path.resolve(__dirname, '.\\files-copy'), (err, files) => {
+          if (err) throw err;
+          files.forEach((file) => {
+            fs.unlink(path.resolve(__dirname, '.\\files-copy', file), (err) => {
+              if (err) throw err;
+            });
+          });
+        });
+
+        fs.readdir(path.resolve(__dirname, '.\\files'), (err, files) => {
+          if (err) console.log(err);
+          else {
+            files.forEach((file) => {
+              fs.copyFile(
+                path.resolve(__dirname, '.\\files', file),
+                path.resolve(__dirname, '.\\files-copy', file),
+                (err) => {
+                  if (err) throw err;
+                }
+              );
+            });
+          }
+        });
+      }
+    }
+  );
+};
+
+copyFolder();
