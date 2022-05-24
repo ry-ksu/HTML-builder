67873734e066ce74e204cf2528838f053a23e837
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-05-24 23:24:33 +0300

    fix: add 'promises' and 'await' for some functions for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,40 +1,36 @@
 const path = require('path');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 
-const copyDir = () => {
-  fs.mkdir(
-    path.join(__dirname, '.\\files-copy'),
-    { recursive: true },
-    (err) => {
-      if (err) {
+const path_files_copy = path.resolve(__dirname, '.\\files-copy');
+
+async function copyDir() {
+  try {
+    await fsPromises.rm(path_files_copy, { recursive: true, force: true });
+  } catch (err) {
+    throw err;
+  }
+
+  try {
+    await fsPromises.mkdir(path_files_copy, { recursive: true });
+  } catch (err) {
+    throw err;
+  }
+
+  try {
+    const files = await fsPromises.readdir(path.resolve(__dirname, '.\\files'));
+    for (const file of files) {
+      try {
+        fsPromises.copyFile(
+          path.resolve(__dirname, '.\\files', file),
+          path.resolve(__dirname, '.\\files-copy', file)
+        );
+      } catch (err) {
         throw err;
-      } else {
-        fs.readdir(path.resolve(__dirname, '.\\files-copy'), (err, files) => {
-          if (err) throw err;
-          files.forEach((file) => {
-            fs.unlink(path.resolve(__dirname, '.\\files-copy', file), (err) => {
-              if (err) throw err;
-            });
-          });
-        });
-
-        fs.readdir(path.resolve(__dirname, '.\\files'), (err, files) => {
-          if (err) console.log(err);
-          else {
-            files.forEach((file) => {
-              fs.copyFile(
-                path.resolve(__dirname, '.\\files', file),
-                path.resolve(__dirname, '.\\files-copy', file),
-                (err) => {
-                  if (err) throw err;
-                }
-              );
-            });
-          }
-        });
       }
     }
-  );
-};
+  } catch (err) {
+    throw err;
+  }
+}
 
 copyDir();
