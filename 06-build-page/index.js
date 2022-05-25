0c95bb32df52359d31ea36a4f8f20c0eb9096618
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-05-25 20:16:26 +0300

    fix: correct path for 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,18 +1,18 @@
 const path = require('path');
 const fsPromises = require('fs').promises;
 
-const pathAssets = path.resolve(__dirname, '.\\project-dist', '.\\assets');
+const pathAssets = path.resolve(__dirname, 'project-dist', 'assets');
 
 async function createIndexHtml() {
   try {
-    await fsPromises.mkdir(path.resolve(__dirname, '.\\project-dist'), { recursive: true });
+    await fsPromises.mkdir(path.resolve(__dirname, 'project-dist'), { recursive: true });
     try {
       let dataTemplate = await fsPromises.readFile(path.resolve(__dirname, 'template.html'), 'utf-8');
       try {
-        const files = await fsPromises.readdir(path.resolve(__dirname, '.\\components'));
+        const files = await fsPromises.readdir(path.resolve(__dirname, 'components'));
         for (let file of files) {
           try {
-            let dataFile = await fsPromises.readFile(path.resolve(__dirname, '.\\components', file));
+            let dataFile = await fsPromises.readFile(path.resolve(__dirname, 'components', file));
             let fileName = file.split('.')[0];
             dataTemplate = dataTemplate.replace(`{{${fileName}}}`, dataFile);
           } catch (err) {
@@ -20,7 +20,7 @@ async function createIndexHtml() {
           }
         }
         try {
-          fsPromises.writeFile(path.join(__dirname, '.\\project-dist', 'index.html'), dataTemplate);
+          fsPromises.writeFile(path.resolve(__dirname, 'project-dist', 'index.html'), dataTemplate);
         } catch (err) {
           throw err;
         }
@@ -37,13 +37,13 @@ async function createIndexHtml() {
 
 async function createStyles() {
   try {
-    const filesStyle = await fsPromises.readdir(path.resolve(__dirname, '.\\styles'), { withFileTypes: true });
+    const filesStyle = await fsPromises.readdir(path.resolve(__dirname, 'styles'), { withFileTypes: true });
     let fileStyleBundle = '';
     for (let file of filesStyle) {
       try {
         if (path.extname(file.name) === '.css' && file.isFile() === true) {
           try {
-            let fileStyle = await fsPromises.readFile(path.join(__dirname, '.\\styles', file.name), 'utf-8');
+            let fileStyle = await fsPromises.readFile(path.resolve(__dirname, 'styles', file.name), 'utf-8');
             fileStyleBundle += fileStyle;
           } catch (err) {
             throw err;
@@ -77,17 +77,17 @@ async function createAssets() {
   }
 
   try {
-    const filesAssets = await fsPromises.readdir(path.resolve(__dirname, '.\\assets'), { withFileTypes: true });
+    const filesAssets = await fsPromises.readdir(path.resolve(__dirname, 'assets'), { withFileTypes: true });
     for (let fileAssets of filesAssets) {
       if (!fileAssets.isFile()) {
         try {
-          await fsPromises.mkdir(path.resolve(__dirname, '.\\project-dist', '.\\assets', fileAssets.name));
-          const filesAssetsDir = await fsPromises.readdir(path.resolve(__dirname, '.\\assets', fileAssets.name), { withFileTypes: true });
+          await fsPromises.mkdir(path.resolve(__dirname, 'project-dist', 'assets', fileAssets.name));
+          const filesAssetsDir = await fsPromises.readdir(path.resolve(__dirname, 'assets', fileAssets.name), { withFileTypes: true });
           for (let fileAssetsDir of filesAssetsDir) {
             try {
               fsPromises.copyFile(
-                path.resolve(__dirname, '.\\assets', fileAssets.name, fileAssetsDir.name),
-                path.resolve(__dirname, '.\\project-dist', '.\\assets', fileAssets.name, fileAssetsDir.name)
+                path.resolve(__dirname, 'assets', fileAssets.name, fileAssetsDir.name),
+                path.resolve(__dirname, 'project-dist', 'assets', fileAssets.name, fileAssetsDir.name)
               );
             } catch (err) {
               throw err;
